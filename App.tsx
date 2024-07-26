524bbc1ff2f3b1ec15cee1392aa2e0abdbd1ba52
Sadaffb <165960513+Sadaffb@users.noreply.github.com>
2024-07-26 11:14:48 +0330

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,27 +1,32 @@
 import React from 'react';
 import "./style.css";
 import {BrowserRouter, Route, Router, Routes, useNavigate ,useParams} from 'react-router-dom';
-import { Password } from './Password';
-import { Login } from './login';
+import { Password } from './password';
 
 
-
-export const App = () => {
-  let navigate = useNavigate(); 
-  const routeChange = () =>{ 
-    let path = `/login`; 
-    navigate(path);
+function Root(){const navigate = useNavigate()
+  
+  const gotToNewPage=()=>{
+    navigate("/Password");
   }
+return (
+  <div className="main_div">
+          <div className='main_title'> ورود | ثبت نام</div>
+          <div className='description'>لطفا شماره موبایل خود را وارد کنید</div>
+          <input className="box_style_phone_number" type="text" placeholder="شماره موبایل"/>
+          <button typeof='' className="button_style_login" 
+          onClick={() => gotToNewPage()}>ثبت</button>
+              <Routes>
+              <Route path="/password" element={<Password />} />
+              </Routes>
+  </div>
+);
+}
+export const App = () => {
   return (
-    
-    <div className="main_div">
-      <Router>
-          <div>
-            <button typeof='' className="button_style_login" onClick={routeChange}>ورود</button>
-          </div>
-          <Route path="/login" element={<Login />} />
-      </Router>
-    </div>
+    <BrowserRouter>
+      <Root />
+    </BrowserRouter>    
   );
 }
 
